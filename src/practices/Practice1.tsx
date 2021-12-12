90b27cbad9394a7e3a86c2c57055dd68abea9893
shn's works <53104737+shnlog@users.noreply.github.com>
2021-12-12 09:25:31 +0900

    返却値の型指定

--- src/practices/Practice1.tsx
+++ src/practices/Practice1.tsx
@@ -8,7 +8,7 @@ export const Practice1 = () => {
 
   return (
     <div>
-      <p>練習問題1:引数の型指定</p>
+      <p>練習問題:引数の型指定</p>
       <button onClick={onClickPractice}>練習問題1を実行</button>
     </div>
   );
