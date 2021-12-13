4456eb2583c1bcaf31cb2bf0f5d0a16391380748
shn's works <53104737+shnlog@users.noreply.github.com>
2021-12-13 21:59:44 +0900

    型定義の管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import { Practice1 } from "./practices/Practice1";
-import { Practice2 } from "./practices/Practice2";
-import { Practice3 } from "./practices/Practice3";
-import { Practice4 } from "./practices/Practice4";
+import axios from "axios";
+import { useState } from "react";
+// import { Practice1 } from "./practices/Practice1";
+// import { Practice2 } from "./practices/Practice2";
+// import { Practice3 } from "./practices/Practice3";
+// import { Practice4 } from "./practices/Practice4";
 import "./styles.css";
+import { Todo } from "./practices/Todo";
+import { TodoType } from "./types/todo";
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
   return (
+    // <div className="App">
+    //   <Practice1 />
+    //   <Practice2 />
+    //   <Practice3 />
+    //   <Practice4 />
+    // </div>
     <div className="App">
-      <Practice1 />
-      <Practice2 />
-      <Practice3 />
-      <Practice4 />
+      <button onClick={onClickFetchData}>データを取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
